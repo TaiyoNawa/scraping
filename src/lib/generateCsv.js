2b387d8df9593edd60811d6b162810c2c35b32be
Makoto Inomata <inomar1486@gmail.com>
2022-05-13 12:38:27 +0900

    fix: prettierを導入し、format

--- src/lib/generateCsv.js
+++ src/lib/generateCsv.js
@@ -3,9 +3,9 @@ const csvStringfySync = require('csv-stringify/sync');
 const path = require('path');
 
 module.exports = (data, fileName, outputhdir = 'output') => {
-	if (!fs.existsSync(path.join(outputhdir))) fs.mkdirSync(outputhdir);
-	const csvString = csvStringfySync.stringify(data, {
-		header: true,
-	});
-	fs.writeFileSync(fileName, csvString);
+  if (!fs.existsSync(path.join(outputhdir))) fs.mkdirSync(outputhdir);
+  const csvString = csvStringfySync.stringify(data, {
+    header: true,
+  });
+  fs.writeFileSync(fileName, csvString);
 };
--- src/workspace/sample-level1.js
+++ src/workspace/sample-level1.js
@@ -8,30 +8,30 @@ const baseUrl = 'http://www.photonext.jp/exhivition';
 const fileName = 'sample-level1.csv';
 
 const options = {
-	transform: (body) => {
-		return cheerio.load(body);
-	},
+  transform: (body) => {
+    return cheerio.load(body);
+  },
 };
 
 (async () => {
-	try {
-		const $ = await rp.get(baseUrl, options);
-		const $items = $('#Containerpocsb .font_8');
-		const values = $items.map((index, value) => {
-			const name = $(value).children('span').find('a').text();
-			const url = $(value).children('span').find('a').attr('href');
-			if (name !== '') {
-				return {
-					name,
-					url,
-					exhibitionName:
-						'10thAniversary PHOTONEXT2019 フォトグラファー&フォトビジネスフェア',
-				};
-			}
-		});
-		console.log(Array.from(values));
-		toCsv(Array.from(values), path.join('output', fileName));
-	} catch (error) {
-		console.error(error);
-	}
+  try {
+    const $ = await rp.get(baseUrl, options);
+    const $items = $('#Containerpocsb .font_8');
+    const values = $items.map((index, value) => {
+      const name = $(value).children('span').find('a').text();
+      const url = $(value).children('span').find('a').attr('href');
+      if (name !== '') {
+        return {
+          name,
+          url,
+          exhibitionName:
+            '10thAniversary PHOTONEXT2019 フォトグラファー&フォトビジネスフェア',
+        };
+      }
+    });
+    console.log(Array.from(values));
+    toCsv(Array.from(values), path.join('output', fileName));
+  } catch (error) {
+    console.error(error);
+  }
 })();
