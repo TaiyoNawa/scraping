94de91fac67461fe0e5e4c8a8c31b9155d207e49
TaiyoNawa <t.nawa0222@gmail.com>
2024-10-11 01:40:00 +0900

    2024/10/11_自動でスクロールさせる関数を追加

--- src/workspace/my/Amazon_最新リリース本ランキング.js
+++ src/workspace/my/Amazon_最新リリース本ランキング.js
@@ -3,14 +3,15 @@ docker-compose build
 docker-compose run --rm app bash
 node src/workspace/... */
 
-//URL:https://www.nissan-stadium.jp/sp/calendar/
+//☆の評価が入っているときはtemp_div[]がずれるので、そこに対応できるようにする
+
 const { launchBrowser, displayLog } = require('./../../lib/browser');
 const fs = require('fs');//ファイル操作に関する関数を利用可能に
 const path = require('path');//パスの結合、解析、正規化などの操作が可能に
 const { stringify } = require('csv-stringify/sync');//CSV形式のデータを文字列に変換するモジュール
 const parse = require('csv-parse/sync');//CSV形式の文字列を解析してJavaScriptのオブジェクトに変換するためのモジュール
 const puppeteer = require('puppeteer');//headless:newにするためにとりあえずインポート
-const outputdir = "my-output/日産スタジアム";//scraping/　以降のパスを指定。
+const outputdir = "my-output/Amazon/本の最新リリースランキング";//scraping/　以降のパスを指定。
 
 
 (async () => {
@@ -29,37 +30,36 @@ const outputdir = "my-output/日産スタジアム";//scraping/　以降のパ�
        // browser = await launchBrowser();
        browser = await launchBrowser();
        await new Promise(resolve => setTimeout(resolve, 5000)); //最初のブラウザ立ち上げから5秒待つ（これしないと最初の行が取得できない）
-       month_english = ["","x","a"]
-       months = [10, 11, 12]
-       
+
       //スクレイピングする月の数だけループする
-      for (let i = 0; i < month_english.length; i++) {
+      for (let i = 1; i <= 2; i++) {
         try {
           const page = await browser.newPage();
-          const URL = `https://www.nissan-stadium.jp/sp/calendar/?m=${month_english[i]}`
-          const month = months[i]
+          const URL = `https://www.amazon.co.jp/gp/new-releases/books/ref=zg_bsnr_pg_2_books?ie=UTF8&pg=${i}`
           await page.goto(URL, { timeout: 10000 });
           try {
 
           //クラス名.data-loadedが表示されるまで待つ
-          await page.waitForSelector('.mds_red_bg', {timeout: 10000});
+          // await page.waitForSelector('#gridItemRoot', {timeout: 6000});
           // ページを最後までスクロール
-          // await autoScroll(page)
-          const result = await page.evaluate((month) => {
+          await autoScroll(page);
+          const result = await page.evaluate(() => {
             const results = []
-            const a = [...document.querySelectorAll(".calendartable01 tr")];
-            a.forEach(row => {
-              const 日 = row.querySelector("th").innerText;
-              const 曜日 = row.querySelector(".th01").innerText;
-              let イベント名 = row.querySelector("td")?.innerText.replace(/\n/g, "");
-              if (日) {
-                results.push({月: month, 日, 曜日, イベント名});
-              } else {
-                results[results.length - 1].イベント名 = イベント名;
-              }
+            a = [...document.querySelectorAll("#gridItemRoot")]
+
+            b = a.forEach(i => {
+                temp = i.querySelector(".zg-grid-general-faceout div div:nth-child(2) div")
+                name = temp.querySelector("a").innerText
+                url = "https://www.amazon.co.jp" + temp.querySelector("a").getAttribute("href")
+                temp_div = temp.querySelectorAll(".a-row")
+                auther = temp_div[0].innerText
+                category = temp_div[1].innerText
+                price = temp_div[2].querySelector("div:first-child")?.innerText
+                point = temp_div[2].querySelector("div:nth-child(2)")?.innerText
+                if(name) results.push({name,url,auther,category,price,point})
             });
             return results;
-          }, month); // month変数を引数として渡す
+          }); // month変数を引数として渡す
           console.log(result)
           all_results.push(...result);  // 配列を展開して結果を追加
         } catch (error) {
@@ -73,7 +73,7 @@ const outputdir = "my-output/日産スタジアム";//scraping/　以降のパ�
     }
       const outputData = stringify(all_results, { header: true });
       try {
-        fs.writeFileSync(`${outputdir}/日産スタジアムイベント一覧(${months.join(",")}月).csv`, outputData, {
+        fs.writeFileSync(`${outputdir}/Amazon_本の最新リリースランキング.csv`, outputData, {
           encoding: 'utf8',
         });
         console.log("ファイルが正常に保存されました");
@@ -89,13 +89,12 @@ const outputdir = "my-output/日産スタジアム";//scraping/　以降のパ�
     }    
   })();
 
-
 // ページを最後までスクロールする関数
 async function autoScroll(page) {
   await page.evaluate(async () => {
       await new Promise((resolve, reject) => {
           let totalHeight = 0;
-          const distance = 100; // スクロールする距離
+          const distance = 100; // スクロールする距離(px)
           const timer = setInterval(() => {
               window.scrollBy(0, distance);
               totalHeight += distance;
